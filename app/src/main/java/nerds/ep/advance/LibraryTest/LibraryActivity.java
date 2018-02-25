c0f33c233350e146ee0ba34778663dd2da274518
hadi <s.h.bnfm.itman@gmail.com>
2018-02-26 01:18:38 +0330

    101 shamsi date added

--- app/src/main/java/nerds/ep/advance/LibraryTest/LibraryActivity.java
+++ app/src/main/java/nerds/ep/advance/LibraryTest/LibraryActivity.java
@@ -2,19 +2,24 @@ package nerds.ep.advance.LibraryTest;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import ep.nerds.eplibrary.EPLib;
 import ep.nerds.eplibrary.utils.BaseActivity;
+import ep.nerds.eplibrary.utils.views.EPImageView;
 import nerds.ep.R;
 import nerds.ep.advance.utils.PublicMethods;
 
 @EActivity(R.layout.activity_library)
 
 public class LibraryActivity extends BaseActivity {
+    @ViewById
+    EPImageView img;
 
     @AfterViews
     void onInit(){
-      PublicMethods.showToast(this ,EPLib.getName() );
+      PublicMethods.showToast(this ,EPLib.getShamsiDate());
+      img.loadURL("https://www.google.com/logos/doodles/2018/2018-doodle-snow-games-day-17-6585325022347264-s.png");
 
     }
 
--- eplibrary/src/main/java/ep/nerds/eplibrary/EPLib.java
+++ eplibrary/src/main/java/ep/nerds/eplibrary/EPLib.java
@@ -5,6 +5,8 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import ir.hamsaa.persiandatepicker.util.PersianCalendar;
+
 /**
  * Created by hadi on 22/02/2018.
  */
@@ -19,6 +21,11 @@ public class EPLib {
     public static void loadImage(Context mContext, String url, ImageView img){
         if (url != null)
             Glide.with(mContext).load(url).into(img);
+    }
+
+    public static String getShamsiDate(){
+        PersianCalendar pc = new PersianCalendar();
+        return pc.getPersianShortDate();
     }
 
 }
